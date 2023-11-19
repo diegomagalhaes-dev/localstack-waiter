482aca6d39723bf7daecd48e271839bc9cff1df8
diego magalhaes <digengup@gmail.com>
2023-11-19 17:33:15 -0300

    feat: config express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World")
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// simple get route for test
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
